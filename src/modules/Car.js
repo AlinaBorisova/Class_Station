141b077207789297ef791bb77923a72f57f0bf95
Алина Борисова <b-alina1999.borisova@yandex.ru>
2023-06-08 17:17:10 +0300

    init

---
+++ src/modules/Car.js
@@ -0,0 +1,37 @@
+class Car {
+    #maxTank; //Скрыть свойство
+    constructor(brand, model, maxTank) {
+        this.brand = brand;
+        this.model = model;
+        this.#maxTank = maxTank;
+        this.nowTank = Math.floor(Math.random() * maxTank);
+    }
+
+    getTitle() {
+        return `${this.brand} ${this.model}`;
+    }
+
+    setModel(model) {
+        this.model = model;
+        return this;
+    }
+
+    needPetrol() {
+        return this.#maxTank - this.nowTank;
+    }
+
+    fillUp() {
+        this.nowTank = this.#maxTank;
+        return this;
+    }
+
+    get maxTank() {
+        return this.#maxTank;
+    }
+
+    set maxTank(data) {
+        console.log(`Нельзя менять значение на ${data}`);
+    }
+}
+
+export const opel = new Car('Opel', 'Crossland', 45);
